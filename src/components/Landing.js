52d26bace889e9a60efb5b6f18fd4cb50453e2e6
duyhong <duyhong@berkeley.edu>
2018-09-26 11:21:58 -0700

    put assets folder in both src and public folders

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,20 +3,23 @@ import React from 'react';
 
 const Landing = () => (
   <section className="landing">
-    <section className="hero-content"> 
+    <section className="hero-content">
       <h1 className="hero-title">Turn the music up!</h1>
     </section>
     <section className="selling-points">
       <div className="point">
-        <h2 className="point-title">Choose your music</h2>
+        <span class="ion-music-note"></span>
+        <h5 className="point-title">Choose your music</h5>
         <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
       </div>
       <div className="point">
-        <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+        <span class="ion-radio-waves"></span>
+        <h5 className="point-title">Unlimited, streaming, ad-free</h5>
         <p className="point-description">No arbitrary limits. No distractions.</p>
       </div>
       <div className="point">
-        <h2 className="point-title">Mobile enabled</h2>
+        <span class="ion-iphone"></span>
+        <h5 className="point-title">Mobile enabled</h5>
         <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
       </div>
     </section>
